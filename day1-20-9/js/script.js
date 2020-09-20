32f5a828a794877110bef99a97c6534215d4afa0
saad0259 <saadshafiq259@gmail.com>
2020-09-20 16:00:42 +0500

    added a cube

--- day1-20-9/js/script.js
+++ day1-20-9/js/script.js
@@ -9,6 +9,7 @@ camera.lookAt( 0, 0, 0 );
 var scene = new THREE.Scene();
 
 var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+var material2 = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 
 var points = [];
 //points.push( new THREE.Vector3( - 10, 5, 0 ) );
@@ -24,16 +25,27 @@ points.push( new THREE.Vector3( -7.5, -10, 0 ) );
 
 
 var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var g2= new THREE.BoxGeometry();
 
 var line = new THREE.Line( geometry, material );
+var cube = new THREE.Mesh(g2,material2);
+
+cube.position.x=25;
+cube.position.y=-3;
+cube.scale.x=15;
+cube.scale.y=15;
+cube.scale.z=15;
+
 
 scene.add( line );
+scene.add(cube);
 var animate = function () {
     requestAnimationFrame( animate );
 
 
     line.rotation.y += 0.1;
-    //line.rotation.x+= 0;
+    cube.rotation.x+= 0.01;
+    cube.rotation.y+= 0.01;
 
     renderer.render( scene, camera );
 };
